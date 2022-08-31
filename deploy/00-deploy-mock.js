b0e87d5af0c578b834269a285673860aeedf99c2
Glenn <glenou@yahoo.com>
2022-08-31 15:53:02 +0800

    deploy mocks

--- deploy/00-deploy-mock.js
+++ deploy/00-deploy-mock.js
@@ -1,10 +1,22 @@
 const {network} = require("hardhat")
-
+const {developmentChains,DECIMALS,INITIAL_ANSWER} = require("../helper-hardhat-config")
 module.exports = async ({
     getNamedAccounts,
     deployments,
 }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const getChainId = network.config.chainId;
+    if(developmentChains.includes(network.name)){
+        log("local nwtwork detectd network detected, deploy mock....")
+        await deploy('MockV3Aggregator', {
+            from: deployer,
+            gasLimit: 4000000,
+            args: [DECIMALS,INITIAL_ANSWER],
+            log: true
+          });
+
+          log("mock deployed!")
+    }
 }
+
+module.exports.tags = ["all","mocks"]
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,13 +4,20 @@ const networkConfig = {
         name: "Gnosis",
         ethUsdPriceFeed: "0xa767f745331D267c7751297D982b050c93985627"
     },
-    137:{
-        name:"polygon",
+    137: {
+        name: "polygon",
         ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945"
     }
 
 }
+const developmentChains = ["hardhat", "localhost"]
 
-module.exports={
+const DECIMALS = 8
+const INITIAL_ANSWER = 200000000000
+module.exports = {
     networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER
+
 }
