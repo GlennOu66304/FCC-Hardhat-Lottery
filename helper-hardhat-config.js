c8cb35bd862f7cd7f128d2a2fb11fdc988947650
Glenn <glenou@yahoo.com>
2022-08-31 15:12:18 +0800

    helpconfig

---
+++ helper-hardhat-config.js
@@ -0,0 +1,16 @@
+
+const networkConfig = {
+    100: {
+        name: "Gnosis",
+        ethUsdPriceFeed: "0xa767f745331D267c7751297D982b050c93985627"
+    },
+    137:{
+        name:"polygon",
+        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945"
+    }
+
+}
+
+module.exports={
+    networkConfig,
+}
